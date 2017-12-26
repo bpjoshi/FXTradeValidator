e9d6a9f9fcaa6b24ebbd059c50a23735e9a43b8e
Bhagwati Prasad <write2bpj@gmail.com>
2017-12-26 22:48:31 +0530

    Annotation for Comments

---
+++ src/main/java/com/bpjoshi/tradevalidator/config/ClassInfo.java
@@ -0,0 +1,15 @@
+package com.bpjoshi.tradevalidator.config;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ClassInfo{
+	String author() default "bpjoshi(Bhagwati Prasad)";
+	int version() default 1;
+	String brief();
+}
